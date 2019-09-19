07f774948a7e21d621c9edfdfb393aac7a90caaf
Keith Wilson <c00205321@itcarlow.ie>
2019-09-19 10:06:04 +0100

    First Commit

---
+++ Assets/Scripts/Xor 21.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Xor21
+{
+    public void Xor21f()
+    {
+        int[] input = { 4, 2, 8, 3, 9, 4, 10 };
+        int[] output = new int[input.Length];
+
+        int temp = 0;
+
+        for (int i = 0; i < input.Length; i++ )
+        {
+            if(input[i]%2 == 0)
+            {
+                temp = (3 *input[i]) + 1;
+            }
+
+            else
+            {
+                temp = input[i] / 2;
+            }
+
+            output[i] = temp;
+
+        }
+
+    }
+}
